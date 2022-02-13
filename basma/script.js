64cdef6fb7140c6297152214c8eb7577569445a5
Animesh-sudo <63156172+Animesh-sudo@users.noreply.github.com>
2022-02-13 21:35:24 +0530

    adding waypoint

--- basma/script.js
+++ basma/script.js
@@ -1,3 +1,5 @@
+// import Waypoint from './waypoint/lib/noframework.waypoints'
+
 function splitScroll(){
     const controller = new ScrollMagic.Controller();
 
@@ -14,27 +16,69 @@ function splitScroll(){
 splitScroll();
 
 
-//Event-listner
+// var waypoint = new Waypoint({
+//     element: document.getElementById('first'),
+//     handler: function() {
+//       console.log("working 1")
+//     }
+//   })
 
-window.onscroll = function () {
-    scrollFunction();
-};
+//   var waypoint1 = new Waypoint({
+//     element: document.getElementById('second'),
+//     handler: function() {
+//       console.log("working 2")
+//     }
+//   })
 
-function scrollFunction() {
-    var image = document.getElementById("myImage");
-    if (document.documentElement.scrollTop > 2500 && document.documentElement.scrollTop <3300 ){
-        image.src = "assests/basma1.png";
-        image.classList.add("animate__animated", "animate__fadeIn");
-    }
-    else if(document.documentElement.scrollTop > 3300 && document.documentElement.scrollTop <4100){
-        image.src = "assests/basma2.png";
-        image.classList.add("animate__animated", "animate__fadeIn");
-    } else if(document.documentElement.scrollTop > 4100){
-        image.src = "assests/basma7.png";
-        image.classList.add("animate__animated", "animate__fadeIn");
-    }
-    else{
-        image.src = "assests/chap3.png";
-        image.classList.add("animate__animated", "animate__fadeIn");
-    }
-}
+//   var waypoint2 = new Waypoint({
+//     element: document.getElementById('third'),
+//     handler: function() {
+//       console.log("working 3")
+//     }
+//   })
+
+//   var waypoint3 = new Waypoint({
+//     element: document.getElementById('fourth'),
+//     handler: function() {
+//       console.log("working 4")
+//     }
+//   })
+
+
+
+
+
+
+
+// //Event-listner
+
+// window.onscroll = function () {
+//     scrollFunction();
+// };
+
+// function scrollFunction() {
+//     var image = document.getElementById("myImage");
+//     if (document.documentElement.scrollTop > 2500 && document.documentElement.scrollTop <3300 ){
+//         image.src = "assests/basma1.png";
+//         image.classList.add("animate__animated", "animate__fadeIn");
+//         image.classList.remove("animate__animated", "animate__fadeIn");
+//         console.log(image.classList);
+//     }
+//     else if(document.documentElement.scrollTop > 3300 && document.documentElement.scrollTop <4100){
+//         // image.classList.remove("animate__animated", "animate__fadeIn");
+        
+//         image.src = "assests/basma2.png";
+//         image.classList.add("animate__animated", "animate__fadeIn");
+//         image.classList.add("animate__animated", "animate__fadeIn");
+        
+//     } else if(document.documentElement.scrollTop > 4100){
+//         // image.classList.remove("animate__animated", "animate__fadeIn");
+//         image.src = "assests/basma7.png";
+//         image.classList.add("animate__animated", "animate__fadeIn");
+//     }
+//     else{
+//         // image.classList.remove("animate__animated", "animate__fadeIn");
+//         image.src = "assests/chap3.png";
+//         image.classList.add("animate__animated", "animate__fadeIn");
+//     }
+// }
