22dfd5b82d8d5a3db4240f108eef57ae79987b7e
Animesh-sudo <63156172+Animesh-sudo@users.noreply.github.com>
2022-02-13 11:49:32 +0530

    changing images with scroll

--- basma/script.js
+++ basma/script.js
@@ -12,3 +12,29 @@ function splitScroll(){
 }
 
 splitScroll();
+
+
+//Event-listner
+
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    var image = document.getElementById("myImage");
+    if (document.documentElement.scrollTop > 2500 && document.documentElement.scrollTop <3300 ){
+        image.src = "assests/basma1.png";
+        image.classList.add("animate__animated", "animate__fadeIn");
+    }
+    else if(document.documentElement.scrollTop > 3300 && document.documentElement.scrollTop <4100){
+        image.src = "assests/basma2.png";
+        image.classList.add("animate__animated", "animate__fadeIn");
+    } else if(document.documentElement.scrollTop > 4100){
+        image.src = "assests/basma7.png";
+        image.classList.add("animate__animated", "animate__fadeIn");
+    }
+    else{
+        image.src = "assests/chap3.png";
+        image.classList.add("animate__animated", "animate__fadeIn");
+    }
+}
