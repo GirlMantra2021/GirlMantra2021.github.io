66bf9a1d657b0d1c8c1687344f247441f8bb4635
Animesh-sudo <63156172+Animesh-sudo@users.noreply.github.com>
2022-02-18 21:50:29 +0530

    removing some comments

--- pregnancy-and-natural-birth-preparation/script.js
+++ pregnancy-and-natural-birth-preparation/script.js
@@ -1,4 +1,3 @@
-// import Waypoint from './waypoint/lib/noframework.waypoints'
 
 function splitScroll(){
     const controller = new ScrollMagic.Controller();
@@ -20,12 +19,10 @@ function playvid(){
         myVideo.play();
         myVideo.controls = true;
         $('.c1').hide();
-        // console.log('Video Playing');
     } else {
         myVideo.pause();
         myVideo.controls = false;
         $('.c1').show();
-        // console.log('Video Paused');
     }
 };
 function check(){
@@ -102,35 +99,3 @@ function myFunction() {
   }
 
 
-// //Event-listner
-
-// window.onscroll = function () {
-//     scrollFunction();
-// };
-
-// function scrollFunction() {
-//     var image = document.getElementById("myImage");
-//     if (document.documentElement.scrollTop > 2500 && document.documentElement.scrollTop <3300 ){
-//         image.src = "assests/basma1.png";
-//         image.classList.add("animate__animated", "animate__fadeIn");
-//         image.classList.remove("animate__animated", "animate__fadeIn");
-//         console.log(image.classList);
-//     }
-//     else if(document.documentElement.scrollTop > 3300 && document.documentElement.scrollTop <4100){
-//         // image.classList.remove("animate__animated", "animate__fadeIn");
-        
-//         image.src = "assests/basma2.png";
-//         image.classList.add("animate__animated", "animate__fadeIn");
-//         image.classList.add("animate__animated", "animate__fadeIn");
-        
-//     } else if(document.documentElement.scrollTop > 4100){
-//         // image.classList.remove("animate__animated", "animate__fadeIn");
-//         image.src = "assests/basma7.png";
-//         image.classList.add("animate__animated", "animate__fadeIn");
-//     }
-//     else{
-//         // image.classList.remove("animate__animated", "animate__fadeIn");
-//         image.src = "assests/chap3.png";
-//         image.classList.add("animate__animated", "animate__fadeIn");
-//     }
-// }
