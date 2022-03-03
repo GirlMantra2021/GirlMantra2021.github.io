d93a70286b7b4172c71d876d145192d0a5f11c64
Animesh-sudo <63156172+Animesh-sudo@users.noreply.github.com>
2022-03-03 23:31:52 +0530

    updates

--- seda-sarp/script.js
+++ seda-sarp/script.js
@@ -98,6 +98,44 @@ function myFunction() {
     window.location = "https://www.basmafaris.com";
   }
 
+  var test1 = document.getElementById('test1');
+  var test2 = document.getElementById('test2');
+  var test3 = document.getElementById('test3');
+  var test4 = document.getElementById('test4');
+  var mainVideo = document.getElementById('main-video');
+
+  function removeActive(){
+    test1.classList.remove('active')
+    test2.classList.remove('active')
+    test3.classList.remove('active')
+    test4.classList.remove('active')
+  }
+
+
+  function testimonial1(){
+    removeActive();
+    test1.classList.add('active');
+    mainVideo.src = "https://iframe.videodelivery.net/bf04ea73c2d9ff1fdc7807a7d6ef729a?poster=https%3A%2F%2Fvideodelivery.net%2Fbf04ea73c2d9ff1fdc7807a7d6ef729a%2Fthumbnails%2Fthumbnail.jpg%3Ftime%3D%26height%3D600"
+  }
+
+  function testimonial2(){
+    removeActive();
+    test2.classList.add('active');
+    mainVideo.src = "https://iframe.videodelivery.net/973eb94f8e1e5d2a8b102693150be98c?poster=https%3A%2F%2Fvideodelivery.net%2F973eb94f8e1e5d2a8b102693150be98c%2Fthumbnails%2Fthumbnail.jpg%3Ftime%3D%26height%3D600"
+  }
+
+  function testimonial3(){
+    removeActive();
+    test3.classList.add('active');
+    mainVideo.src = "https://iframe.videodelivery.net/0be582fa2307e5451a72227aa98ac0e9?poster=https%3A%2F%2Fvideodelivery.net%2F0be582fa2307e5451a72227aa98ac0e9%2Fthumbnails%2Fthumbnail.jpg%3Ftime%3D%26height%3D600"
+  }
+
+  function testimonial4(){
+    removeActive();
+    test4.classList.add('active');
+    mainVideo.src = "https://iframe.videodelivery.net/e7a5c5de997124818c5366510c050de9?poster=https%3A%2F%2Fvideodelivery.net%2Fe7a5c5de997124818c5366510c050de9%2Fthumbnails%2Fthumbnail.jpg%3Ftime%3D%26height%3D600"
+  }
+
   // let width = screen.width;
   // console.log(width);
 
